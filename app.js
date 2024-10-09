31bfe9dc03ba9ec7de2cdadb4181118ebf0c34ac
rjuhkam <robert.juhkam7@gmail.com>
2024-10-09 16:13:45 +0300

    views

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
