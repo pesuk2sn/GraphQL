7d672e7c9541a34989ddd4953b6bcbc6b0a82662
rjuhkam <robert.juhkam7@gmail.com>
2024-11-04 16:39:12 +0200

    bar chart and pie chart added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs')
 app.use(express.json());
 app.use(express.static('js'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 async function authentication(username, password){
  
